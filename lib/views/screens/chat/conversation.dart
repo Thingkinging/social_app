9cd0e6c10283e5d1906c72c9386d689a5151bd92
kwh1400 <kwh1400@naver.com>
2023-11-02 19:33:29 +0900

    Commit

--- lib/views/screens/chat/conversation.dart
+++ lib/views/screens/chat/conversation.dart
@@ -1,14 +1,104 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:social_app/util/data.dart';
+
+import '../../widgets/chat_bubble.dart';
 
 class Conversation extends StatefulWidget {
-
   @override
   State<Conversation> createState() => _ConversationState();
 }
 
 class _ConversationState extends State<Conversation> {
+  static Random random = Random();
+  String name = names[random.nextInt(10)];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 3,
+        leading: IconButton(
+          icon: Icon(Icons.keyboard_backspace),
+          onPressed: () => Navigator.pop(context),
+        ),
+        titleSpacing: 0,
+        title: InkWell(
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 0.0, right: 10.0),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "assets/images/cm${random.nextInt(10)}.jpeg",
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Online",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          onTap: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        height: MediaQuery
+            .of(context)
+            .size
+            .height,
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                itemCount: conversation.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Map msg = conversation[index];
+                  return ChatBubble(
+                      message: msg['type'] == "text" ? messages[random.nextInt(
+                          10)] : "assets/images/cm${random.nextInt(10)}.jpeg",
+                    username: msg["username"],
+                    time: msg["time"],
+                    type: msg['type'],
+                    replyText: msg["replyText"],
+                    isMe: msg['isMe'],
+                    isGroup: msg['isGroup'],
+                    isReply: msg['isReply'],
+                    replyName: name,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/views/screens/profile.dart
+++ lib/views/screens/profile.dart
@@ -1,9 +1,132 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Profile extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:social_app/util/data.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  static Random random = Random();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 60),
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  "assets/images/cm${random.nextInt(10)}.jpeg",
+                ),
+                radius: 50,
+              ),
+              SizedBox(height: 10),
+              Text(
+                names[random.nextInt(10)],
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Status should be here",
+                style: TextStyle(),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextButton(
+                    child: Icon(
+                      Icons.message,
+                      color: Colors.white,
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey),
+                    ),
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 10),
+                  TextButton(
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildCategory("Posts"),
+                    _buildCategory("Friends"),
+                    _buildCategory("Groups"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                primary: false,
+                padding: EdgeInsets.all(5),
+                itemCount: 15,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 200 / 200,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Image.asset(
+                      "assets/images/cm${random.nextInt(10)}.jpeg",
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildCategory(String title) {
+    return Column(
+      children: [
+        Text(
+          random.nextInt(10000).toString(),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(
+          title,
+          style: TextStyle(),
+        ),
+      ],
+    );
   }
 }
